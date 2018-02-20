7860f3c617b82387c99a7653a65e3c0eb96b1246
serv-inc <kreikenbaumaccoun@gmail.com>
2018-02-20 12:45:09 +0100

    1

---
+++ app/src/main/java/com/udacity/serv_inc/popmovies/MainActivity.java
@@ -0,0 +1,28 @@
+package com.udacity.serv_inc.popmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    // https://developer.android.com/guide/topics/ui/layout/gridview.html
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
